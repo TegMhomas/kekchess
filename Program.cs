13af6365de425de677b62fb6a463a152a435fa41
TegMhomas <gruszka.patryk@gmail.com>
2020-08-21 16:59:01 +0200

    1
    1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kekchess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
